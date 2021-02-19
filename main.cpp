1a4f14c6d41f193b35b986ab005f1a0fcfd2f6ee
Colliquake <alex.roh01@gmail.com>
2021-02-18 16:00:16 -0800

    Setup

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int x;
+    std::cout << "You are using the 8-Puzzle Solver. To use a default (pre-made) puzzle, press 1. To enter a custom puzzle, press 2." << std::endl;
+    std::cin>> x;
+    std::cout<< x;
+
+}
